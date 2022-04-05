e9aad63302b73ff158baa668b0f6c69792bbcca2
Linding3r <thomasjuullindinger@yahoo.dk>
2022-04-05 09:19:17 +0200

    added Main class with main method

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Dog dog1 = new Dog("Lasse");
+    Cat cat1 = new Cat("Morten");
+
+    cat1.makeSound();
+    dog1.makeSound();
+  }
+}
